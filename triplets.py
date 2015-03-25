9c31667434db9395b74834c86145e1dd9e8f22ae
Josip Domsic <josip.domsic@gmail.com>
2015-03-25 17:26:02 +0100

    initial

---
+++ triplets.py
@@ -0,0 +1,17 @@
+import itertools
+
+def take(n, iterable):
+  "Return first n items of the iterable as a list"
+  return list(itertools.islice(iterable, n))
+
+def consume(n, iterable):
+  "Return items from iterable, starting from n"
+  return list(itertools.islice(tokens, n, None))
+
+tokens = "Isla djevojka kroz sumu kad tamo vuk iskoci na nju".split()
+
+for step in range(len(tokens)):
+    ending = consume(step, tokens)
+    triplet = take(3, consume)
+    print ' '.join(triplet)
+
